cebb2d5bffcf76d9ab15f29699a4e8cb6d513fef
Aurum2112 <117100675+Aurum2112@users.noreply.github.com>
2022-11-29 11:12:46 -0500

    Add files via upload

--- EbirdScrapper.py
+++ EbirdScrapper.py
@@ -1,8 +1,10 @@
 import requests
 import json
 from bs4 import BeautifulSoup
-url = input("Input Ebird.org Url")
-filname = input("Input filename for .json output file")
+import datetime
+
+url = "https://ebird.org/hotspot/L737447?yr=all&m=&rank=hc"
+filname = "data"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 #print(soup.prettify())
@@ -21,10 +23,29 @@ for x in range(0,200):
         birddata = (i.find(class_ = "Observation-meta-date-label"))
         birddata = (birddata.find("span"))
         birddata =(birddata.contents[0])
+        format = '%d %m %Y'
+        birddata = birddata.replace("Jan","1")
+        birddata = birddata.replace("Feb","2")
+        birddata = birddata.replace("Mar","3")
+        birddata = birddata.replace("Apr","4")
+        birddata = birddata.replace("May","5")
+        birddata = birddata.replace("Jun","6")
+        birddata = birddata.replace("Jul","7")
+        birddata = birddata.replace("Aug","8")
+        birddata = birddata.replace("Sep","9")
+        birddata = birddata.replace("Oct","10")
+        birddata = birddata.replace("Nov","11")
+        birddata = birddata.replace("Dec","12")
+        
+        birddate = datetime.datetime.strptime(birddata, format)
+     #   birddate = str(birddate)
+      #  print (birddate)
+        montoday = (30 * birddate.month) - 30
+        birddate = int(birddate.day) + int(montoday)
         #print(birddata)
-        adict = {"name":birdname,"date":birddata, "count":nob}
+        adict = {"name":birdname,"date":birddate, "count":nob}
         nesteddict.append(adict)
-        print(birdname,birddata,nob)
+        print(birdname,birddate,nob)
 
 jsonString = json.dumps(nesteddict, indent=4)
 jsonFile = open((filname + ".json"), "w")
